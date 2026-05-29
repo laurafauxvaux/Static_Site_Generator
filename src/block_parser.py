1b56658e7b46878858044fadfd77c663d5d715d8
laurafauxvaux <laurafauxvaux@outlook.fr>
2026-05-29 11:55:26 +0200

    10. Implemented block_to_block_type function in block_parser.py and its tests

--- src/block_parser.py
+++ src/block_parser.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 def markdown_to_blocks(markdown:str)->list[str]:
     blocks = []
     result = []
@@ -8,3 +10,53 @@ def markdown_to_blocks(markdown:str)->list[str]:
             result.append(stripped_block)
     return result
     
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(markdown_text:str)->BlockType:
+    if markdown_text.startswith("#"):
+        sections = markdown_text.split(" ", 1)
+        if (
+            len(sections) == 2
+            and sections[1] != ""
+            and all(char == "#" for char in sections[0])
+            and 0 < len(sections[0]) < 7
+        ):
+            return BlockType.HEADING
+        return BlockType.PARAGRAPH
+        
+    elif markdown_text.startswith("```\n") and markdown_text.endswith("```"):
+        return BlockType.CODE
+    
+    elif markdown_text.startswith(">"):
+        sections = markdown_text.split("\n")
+        if (
+            all(section.startswith(">") for section in sections)
+            and all(section[1:].strip() != "" for section in sections)
+        ):
+            return BlockType.QUOTE
+        return BlockType.PARAGRAPH
+        
+    elif markdown_text.startswith("- "):
+        sections = markdown_text.split("\n")
+        if all(section.startswith("- ") for section in sections):
+            return BlockType.UNORDERED_LIST
+        return BlockType.PARAGRAPH
+            
+    elif markdown_text.startswith("1. "):
+        sections = markdown_text.split("\n")
+        number = 1
+        for section in sections:
+            if section.startswith(f"{number}. "):
+                number += 1
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+    
+    else:
+        return BlockType.PARAGRAPH
--- src/test_block_parser.py
+++ src/test_block_parser.py
@@ -1,5 +1,7 @@
 import unittest
-from block_parser import markdown_to_blocks
+from block_parser import (markdown_to_blocks, 
+                          block_to_block_type,
+                          BlockType)
 
 class TestBlockParser(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -44,4 +46,62 @@ and an excessive newline
                          ["This is a text with excessive    whitespace"])
     
 
-        
+    def test_block_to_block_type_heading(self):
+        md = "#### This is a heading"
+        self.assertEqual(block_to_block_type(md), BlockType.HEADING)
+    
+    def test_block_to_block_type_invalid_heading(self):
+        md = "####### This is an invalid heading"
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+    
+    def test_block_to_block_type_empty_heading(self):
+        md = "## "
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+    
+    def test_block_to_block_type_code(self):
+        md = """```
+This is a code block
+```"""
+        self.assertEqual(block_to_block_type(md), BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        md = """>This is a quote without whitespace
+> Including whitespace"""
+        self.assertEqual(block_to_block_type(md), BlockType.QUOTE)
+    
+    def test_block_to_block_type_invalid_quote(self):
+        md = """>This is a quote
+And this is not"""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+    
+    def test_block_to_block_type_empty_quote(self):
+        md = "> "
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_unordered_list(self):
+        md = """- This is a list
+- that is unordered"""
+        self.assertEqual(block_to_block_type(md), BlockType.UNORDERED_LIST)
+    
+    def test_block_to_block_type_invalid_unordered(self):
+        md = """- This is a list
+rendered invalid"""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_invalid_ordered(self):
+        md = """1. This is a list
+2. With a numerical order
+99. But the rule is 1 by 1"""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph(self):
+        md = "This is a basic text pararaph."
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+    
+    def test_block_to_block_type_empty_paragraph(self):
+        md = ""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+
+    
+    
